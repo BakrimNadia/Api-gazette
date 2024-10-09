bf24ba47ede39b488298b21a7382142529e7d45e
BakrimNadia <nadia_lina@hotmail.com>
2024-10-09 18:13:58 +0200

    create file database and url database to .env

---
+++ src/database.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import pg from 'pg';
+
+import { Sequelize } from "sequelize";
+
+const database = process.env.PG_URL;
+
+console.log(database);
+console.log(typeof database);
+
+export const sequelize = new Sequelize(database , {
+  dialectModule: pg,
+  define: {
+    timestamps: false,
+  },
+  logging: false,
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+export default sequelize;
