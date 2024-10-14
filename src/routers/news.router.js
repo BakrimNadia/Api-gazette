7431502686da64f3b1b44332288706e3b3bec41c
BakrimNadia <nadia_lina@hotmail.com>
2024-10-14 10:46:16 +0200

    Create news.router and add import to router file

---
+++ src/routers/news.router.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import newsController from '../controllers/news.controller.js';
+
+export const router = Router();
+
+router.get('/news', newsController.getAll);
+router.get('/news/:id', newsController.getById);
+router.post('/news', newsController.insert);
+router.patch('/news/:id', newsController.update);
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,3 +1,6 @@
 import { Router } from 'express';
+import { router as newsRouter } from './news.router.js';
 
 export const router = Router();
+
+router.use(newsRouter);
