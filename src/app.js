604b9bbf6f4f0ebe544fdc8c4ecea52d4369e33d
BakrimNadia <nadia_lina@hotmail.com>
2024-12-17 14:43:24 +0100

    add middlexware sanitize

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { bodySanitizer } from "./middlewares/bodySanitizer.js";
 
 import { router as apiRouter } from "./routers/router.js";
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use(bodySanitizer);
 
 app.get("/", (req, res) => {
   res.json({
---
+++ src/middlewares/bodySanitizer.js
@@ -0,0 +1,10 @@
+import sanitizeHtml from "sanitize-html";
+
+export const bodySanitizer = (req, res, next) => {
+  Object.keys(req.body).forEach(key => { 
+    if (typeof req.body[key] === "string") {
+      req.body[key] = sanitizeHtml(req.body[key]);
+    }
+  });
+  next();
+};
