3fc5b472300304481e8859e5e776f7e74c7da438
BakrimNadia <nadia_lina@hotmail.com>
2024-12-24 18:14:30 +0100

    add try and catch to category controller

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -4,10 +4,16 @@ import sanitizeHtml from "sanitize-html";
 
 const categoryController = {
     getAll : async (req, res) => {
-        const categoryList = await Category.findAll();
-        res.json(categoryList);
+        try {
+            const categoryList = await Category.findAll();
+            res.json(categoryList);
+        } catch (error) {
+            console.error("Erreur lors de la récupération des categories :", error);
+            res.status(500).json({ error: "Erreur serveur" });
+        }
     },
     getById : async (req, res) => {
+        try {
         const categoryId = parseInt(req.params.id);
 
         if (!categoryId) {
@@ -20,6 +26,10 @@ const categoryController = {
             return res.status(404).json({ error: "categorie inconnu" });
         }
         res.json(category);
+    } catch (error) {
+        console.error("Erreur lors de la récupération de la categorie :", error);
+        res.status(500).json({ error: "Erreur serveur" });
+    }
     },
 
     insert :  [sanitizeHtml, async (req, res) => {
@@ -28,16 +38,25 @@ const categoryController = {
             console.log("Tous les champs sont obligatoire");
             return res.status(400).json({ error: "Tous les champs sont obligatoire" });
         }
+
+        try {
         const newCategory = {
             name,
         };
         await Category.create(newCategory);
         res.status(201).json(newCategory);
+    }
+    catch (error) {
+        console.error("Erreur lors de la création de la categorie :", error);
+        res.status(500).json({ error: "Erreur serveur" });
+    }
     }],
 
     update :  [sanitizeHtml, async (req, res) => {
         const categoryId = parseInt(req.params.id);
         const { name } = req.body;
+
+        try {
         if (!categoryId) {
             return res.status(404).json({ error: "id inconnu" });
         }
@@ -51,6 +70,11 @@ const categoryController = {
         category.name = name;
         await category.save();
         res.json(category);
+    }
+    catch (error) {
+        console.error("Erreur lors de la mise à jour de la categorie :", error);
+        res.status(500).json({ error: "Erreur serveur" });
+    }
     }],
 
     delete : async (req, res) => {
@@ -58,13 +82,18 @@ const categoryController = {
         if (!categoryId) {
             return res.status(404).json({ error: "id inconnu" });
         }
+
+        try {
         const category = await Category.findByPk(categoryId);
         if (!category) {
             return res.status(404).json({ error: "categorie inconnu" });
         }
         await category.destroy();
         res.json({ message: "categorie supprimé" });
-    }
+    } catch (error) {
+        console.error("Erreur lors de la suppression de la categorie :", error);
+        res.status(500).json({ error: "Erreur serveur" });
+    }},
 };
 
 export default categoryController;
