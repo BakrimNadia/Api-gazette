5dcb5a526bad4eb29b495356c5193cacdca4dc94
BakrimNadia <nadia_lina@hotmail.com>
2025-01-04 16:57:03 +0100

    chore: modify package json

--- test.hooks.js
+++ test.hooks.js
@@ -6,30 +6,18 @@ let server;
 
 export const mochaHooks = {
   
-  // Function for all tests
   async beforeAll() {
-    // create tables in the database test
     await sequelize.sync();
-
-    // Start the server
     server = await app.listen(process.env.PORT || 3001);
   },
 
-  // Function after tests
   async afterAll() {
-    // close the server
     await server.close();
-
-    // delete all tables in the database test
     await sequelize.drop({ cascade: true, force: true });
-
-    //close the connection to the database test
     await sequelize.close();
   },
 
-  // Function before each test
   async beforeEach() {
-    // delete all data in the tables
     await sequelize.truncate({ cascade: true, force: true });
   }
 };
